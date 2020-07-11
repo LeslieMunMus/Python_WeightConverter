c4b7c22898c4857b006994a9e92710ce8992d682
Leslie Musengi <leslie.musengi@icloud.com>
2020-07-11 17:46:56 +0200

    Initial code completed for weight converter

--- weightConverter.py
+++ weightConverter.py
@@ -1,3 +1,23 @@
 import os
 
 os.system("clear")
+
+# I am using %0.2f to format a float to 2 decimal places
+
+
+def main():
+    weight = float(input("Weight: "))
+    metric_symbol = input("(L)bs or (K)g: ")
+
+    if metric_symbol.upper() == "K":
+        calc = weight / 0.45
+        print("You are %0.2f pounds" % (calc))
+    elif metric_symbol.upper() == "L":
+        calc = weight * 0.45
+        print("You are %0.2f kilograms" % (calc))
+    else:
+        print("The metric symbol you entered is not supported")
+
+
+if __name__ == "__main__":
+    main()
